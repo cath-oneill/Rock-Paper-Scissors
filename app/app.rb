3ab11ecbaff4ff78e75a93c86c780cf598a58ba9
Catherine O'Neill <cathzozo@gmail.com>
2014-08-06 16:56:24 -0500

    improved app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
-require_relative 'lib/puppy-breeder.rb'
+require_relative 'lib/rps.rb'
 
 get '/' do
   erb :index
 end
+
+get '/landing' do
--- app/lib/rps.rb
+++ app/lib/rps.rb
@@ -2,6 +2,7 @@ module RPS_Logic
 end
 
 require_relative 'db/dbi.rb'
+
 require_relative 'rps/user.rb'
 require_relative 'rps/round.rb'
 require_relative 'rps/match.rb'
