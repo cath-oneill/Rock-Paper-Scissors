de4f26a8667693c6cc9da3acceff79f1f596620d
Cristina Caputo <cristinacaputo@gmail.com>
2014-08-06 10:58:51 -0500

    add dbi

--- app/lib/dbi.rb
+++ app/lib/dbi.rb
@@ -40,7 +40,109 @@ module DBI
           start_at timestamp NOT NULL DEFAULT current_timestamp
         )])
     end
+    #
+    # Method to create a user record
+    #
+    def save_user(name, password_digest, email, profile_pic)
+      @db.exec(%q[
+        INSERT INTO users (name, password_digest, email, profile_pic)
+        VALUES ($1, $2, $3, $4);
+      ], [name, password_digest, email, profile_pic])
+    end
 
+
+    def build_user(data)
+     User.new(data["name"], data["password_digest"], data["email"], data["profile_pic"], data["join_at"], data["user_id"])
+    end
+ 
+
+
+    def get_user_by_email(this_email)
+      response = @db.exec("SELECT * FROM users WHERE email = '#{this_email}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_user_by_id(this_id)
+      response = @db.exec("SELECT * FROM users WHERE user_id = '#{this_id}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_all_users
+      response = @db.exec("SELECT * FROM users")
+      response.map {|row| build_user(row)}
+    end
+
+  # Method to create a match record
+    #
+    def save_match(player_1_id, player_2_id)
+      @db.exec(%q[
+        INSERT INTO users (player_1_id, player_2_id)
+        VALUES ($1, $2);
+      ], [player_1_id, player_2_id])
+    end
+
+    def build_match(data)
+      Match.new(data["player_1_id"], data["player_2_id"], data["match_id"], data["start_at"], data["completed"], data["winner_id"])
+    end
+    
+    def get_matches_by_id(this_id)
+      response = @db.exec("SELECT * FROM matches WHERE match_id = '#{this_id}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_finished_matches_by_id(this_id)
+      response = @db.exec("SELECT * FROM matches WHERE match_id = '#{this_id}' AND completed = 't'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_unfinished_matches_by_id(this_id)
+      response = @db.exec("SELECT * FROM matches WHERE match_id = '#{this_id}' AND completed = 'f'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_match_by_player(this_id)
+      response = @db.exec("SELECT * FROM matches WHERE player_1_id = '#{this_id}' OR player_2_id = '#{this_id}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_match_by_winner(this_id)
+      response = @db.exec("SELECT * FROM matches WHERE winner_id = '#{this_id}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_all_matches
+      response = @db.exec("SELECT * FROM matches")
+      response.map {|row| build_user(row)}
+    end
+
+
+  # Method to create a round record
+    def save_round(round_id, match_id)
+      @db.exec(%q[
+        INSERT INTO rounds (round_id, match_id)
+        VALUES ($1, $2);
+      ], [round_id, match_id])
+    end
+
+    def build_round(data)
+      Round.new(data["match_id"], data["round_id"], data["player_1_move"], data["player_2_move"], data["result"])
+    end
+
+    def get_round_by_id(this_id)
+      response = @db.exec("SELECT * FROM rounds WHERE round_id = '#{this_id}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_round_by_match_id(this_id)
+      response = @db.exec("SELECT * FROM rounds WHERE match_id = '#{this_id}'")
+      response.map {|row| build_user(row)}
+    end
+
+    def get_all_rounds
+      response = @db.exec("SELECT * FROM rounds")
+      response.map {|row| build_user(row)}
+    end
+  
     def self.dbi
       @__db_instance ||= RPS.new
     end
